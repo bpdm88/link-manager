da61688cda32d4993ce7a44b821d134fc29d141b
Ben <benjamin.main88@gmail.com>
2021-04-28 18:11:21 +0100

    Get request successful

--- server/routers/linkRouter.js
+++ server/routers/linkRouter.js
@@ -1,28 +1,37 @@
 const router = require('express').Router()
 const Link = require('../models/linkModel')
 
-router.get('/test', (req, res) => {
-  res.send('Router test')
+router.get('/', async (req, res) => {
+  try {
+    const links = await Link.find()
+    res.json(links)
+  } catch (error) {
+    res.status(500).send()
+  }
 })
 
 router.post('/', async (req, res) => {
-  const { title, author, link } = req.body
+  try {
+    const { title, author, link } = req.body
 
-  // validation
+    // validation
 
-  if (!link) {
-    return res.status(400).json({ errorMessage: 'You need to provide a link' })
+    if (!link) {
+      return res.status(400).json({ errorMessage: 'You need to provide a link' })
+    }
+
+    const newLink = new Link({
+      title,
+      author,
+      link
+    })
+
+    const savedLink = await newLink.save()
+
+    res.json(savedLink)
+  } catch (error) {
+    res.status(500).send()
   }
-
-  const newLink = new Link({
-    title,
-    author,
-    link
-  })
-
-  const savedLink = await newLink.save()
-
-  res.json(savedLink)
 })
 
 module.exports = router
