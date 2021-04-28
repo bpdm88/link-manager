337a296ff472b474ed35b5f289dfd702ed9a4399
Ben <benjamin.main88@gmail.com>
2021-04-28 19:35:56 +0100

    put request

--- server/routers/linkRouter.js
+++ server/routers/linkRouter.js
@@ -10,6 +10,61 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const linkId = req.params.id
+
+    // validation
+
+    if (!linkId) {
+      return res.status(400).json({ errorMessage: 'Link id not provided' })
+    }
+
+    const existingLink = await Link.findById(linkId)
+
+    if (!existingLink) {
+      return res.status(400).json({ errorMessage: 'Link id not found' })
+    }
+
+    await existingLink.delete()
+    res.json(existingLink)
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { title, author, link } = req.body
+    const linkId = req.params.id
+
+    // validation
+
+    if (!link) {
+      return res.status(400).json({ errorMessage: 'You need to provide a link' })
+    }
+
+    if (!linkId) {
+      return res.status(400).json({ errorMessage: 'Link id not provided' })
+    }
+
+    const originalLink = await Link.findById(linkId)
+
+    if (!originalLink) {
+      return res.status(400).json({ errorMessage: 'Link id not found' })
+    }
+
+    originalLink.title = title
+    originalLink.author = author
+    originalLink.link = link
+
+    const savedLink = await originalLink.save()
+    res.json(savedLink)
+  } catch (error) {
+    res.status(500).send()
+  }
+})
+
 router.post('/', async (req, res) => {
   try {
     const { title, author, link } = req.body
