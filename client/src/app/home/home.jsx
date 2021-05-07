151958f338d160f545921511890e6bb73abc089c
Ben <benjamin.main88@gmail.com>
2021-05-07 16:11:31 +0100

    delete unfinished

--- client/src/app/home/home.jsx
+++ client/src/app/home/home.jsx
@@ -6,7 +6,9 @@ const Home = ({ links }) => {
   return (
     <div>
       {links.map((link, i) => {
-        return <Link key={i} title={link.title} author={link.author} link={link.link} />
+        return (
+          <Link key={i} title={link.title} author={link.author} link={link.link} id={link._id} />
+        )
       })}
     </div>
   )
--- client/src/components/link/index.jsx
+++ client/src/components/link/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import axios from 'axios'
 
-const Link = ({ title, author, link }) => {
+const Link = ({ title, author, link, id }) => {
+  const handleDelete = async () => {
+    console.log(id)
+  }
+
   return (
     <div>
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   )
 }
