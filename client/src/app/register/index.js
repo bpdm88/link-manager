9832a360e2b14e40ce9da1d140378a30422a9290
Ben <benjamin.main88@gmail.com>
2021-05-25 15:15:04 +0100

    register style

--- client/src/app/register/index.js
+++ client/src/app/register/index.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 import React, { useState, useContext } from 'react'
 import { Link, useHistory } from 'react-router-dom'
+import UserContext from '../../context/UserContext'
 
 import ErrorMessage from '../../components/errorMessage'
+import Input from '../../components/input'
+import Button from '../../components/button'
 
-import UserContext from '../../context/UserContext'
+import Logo from '../../assets/icons/icon-med.svg'
 
 const Register = () => {
   const [formData, setFormData] = useState({ email: '', password: '', passwordVerify: '' })
@@ -37,35 +40,67 @@ const Register = () => {
     }
   }
   return (
-    <div>
-      <form onSubmit={register}>
-        <label htmlFor="form-email">Email</label>
-        <input
-          type="email"
-          id="form-email"
-          onChange={e => setFormData({ ...formData, email: e.target.value })}
-          value={formData.email}
-        />
-        <label htmlFor="form-password">Password</label>
-        <input
-          type="password"
-          id="form-password"
-          onChange={e => setFormData({ ...formData, password: e.target.value })}
-          value={formData.password}
-        />
-        <label htmlFor="verify">Password</label>
-        <input
-          type="password"
-          id="verify"
-          onChange={e => setFormData({ ...formData, passwordVerify: e.target.value })}
-          value={formData.passwordVerify}
-        />
-        <button type="submit">Register</button>
-      </form>
-      {errorMessage && <ErrorMessage message={errorMessage} />}
-      <p>
-        Already have an account <Link to="/">login instead</Link>
-      </p>
+    <div className="login--container">
+      <div className="login--left">
+        <div className="left--content">
+          <div className="login--title">
+            <h1 className="heading--alpha">Shelf</h1>
+            <div>
+              <img src={Logo} />
+            </div>
+          </div>
+          <h2 className="heading--bravo">
+            Manage, Share and <span>Organise</span>
+          </h2>
+          <p>
+            Shelf lets you manage all of your links all in one place wherever it be recipies,
+            articles or design. Set up categories and save any links in an organised manner so you
+            can refer to them again and again!{' '}
+          </p>
+        </div>
+      </div>
+      <div className="login--right">
+        <div className="right--content">
+          <h2 className="login--secondary-title heading--charlie">Register your account:</h2>
+          <form onSubmit={register} className="login--form">
+            <Input
+              label={'Email:'}
+              name="form-email"
+              type="email"
+              id="form-email"
+              onChange={e => setFormData({ ...formData, email: e.target.value })}
+              value={formData.email}
+              src={'user'}
+            />
+            <Input
+              label={'Password:'}
+              name="form-password"
+              type="password"
+              id="form-password"
+              onChange={e => setFormData({ ...formData, password: e.target.value })}
+              value={formData.password}
+              src={'key'}
+            />
+            <Input
+              label={'Verify Password:'}
+              name="verify"
+              type="verify"
+              id="verify"
+              onChange={e => setFormData({ ...formData, passwordVerify: e.target.value })}
+              value={formData.passwordVerify}
+              src={'key'}
+            />
+            <Button type="submit">Register</Button>
+          </form>
+          <div>{errorMessage && <ErrorMessage message={errorMessage} />}</div>
+          <div>
+            <p className="signup--tag">
+              Already a member
+              <Link to="/"> log in here</Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
