899abb3004f25df0ef7b27e7caf2e4ca854492f6
Ben <benjamin.main88@gmail.com>
2021-06-11 14:29:41 +0100

    add netifily to cors origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://shelf-link-manager.netlify.app'],
     credentials: true
   })
 )
