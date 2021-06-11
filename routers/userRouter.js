27e9fc3a2584da70f6c104e1eb2e4d47d2e641f9
Ben <benjamin.main88@gmail.com>
2021-06-11 14:57:20 +0100

    fix to log out cookie

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -135,7 +135,20 @@ router.get('/loggedIn', (req, res) => {
 
 router.get('/logOut', (req, res) => {
   try {
-    res.clearCookie('token').send()
+    res
+      .cookie('token', '', {
+        httpOnly: true,
+        sameSite:
+          process.env.NODE_ENV === 'development'
+            ? 'lax'
+            : process.env.NODE_ENV === 'production' && 'none',
+        secure:
+          process.env.NODE_ENV === 'development'
+            ? false
+            : process.env.NODE_ENV === 'production' && true,
+        exprires: new Date(0)
+      })
+      .send()
   } catch (error) {
     return res.json(null)
   }
